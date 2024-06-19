64e3a95cbbf979f1fc8af2eb78503fce35268269
TAKANO <takanotomoo@gmail.com>
2024-06-19 15:56:50 +0900

    初python作成

---
+++ djangosnippets/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include # include関数の追加でインポート
+
+from snippets.views import top
+
+urlpatterns = [
+    path('', top, name='top'),
+    path('snippets/', include('snippets.urls')), # snippets/urls.pyの読み込み
+    path('admin/', admin.site.urls),
+]
