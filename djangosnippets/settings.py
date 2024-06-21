1ec9ecd87fd8bdb577d2196cfefd26b5a7025373
TAKANO <takanotomoo@gmail.com>
2024-06-21 15:06:22 +0900

    更新0621

--- djangosnippets/settings.py
+++ djangosnippets/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'snippets.apps.SnippetsConfig',
     'django_bootstrap5',
+    'pygments_renderer',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'djangosnippets.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR /'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- djangosnippets/urls.py
+++ djangosnippets/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', top, name='top'),
     path('snippets/', include('snippets.urls')), # snippets/urls.pyの読み込み
     path('admin/', admin.site.urls),
+    path("accounts/", include("accounts.urls")),
 ]
